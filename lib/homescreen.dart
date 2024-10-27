35019d887a56edc5596d0fa86dd999ce8b2f5c76
Ali Hassan <hassan.jutt646464@gmail.com>
2024-10-27 17:10:28 +0500

    first commit

---
+++ lib/homescreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Get X"),
+      ),
+      body: const Column(
+        children: [],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Get.snackbar("Snackbar", " with help of GetX");
+      }),
+    );
+  }
+}
