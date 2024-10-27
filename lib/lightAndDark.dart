6834fb781aa9346f3a2d1b7e08609028f4e919dc
Ali Hassan <hassan.jutt646464@gmail.com>
2024-10-27 17:43:42 +0500

    xyz

---
+++ lib/lightAndDark.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Lightanddark extends StatefulWidget {
+  const Lightanddark({super.key});
+
+  @override
+  State<Lightanddark> createState() => _LightanddarkState();
+}
+
+class _LightanddarkState extends State<Lightanddark> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("data"),
+      ),
+    );
+  }
+}
