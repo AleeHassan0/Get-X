d90c3a081d65599a6359583750bde9502b4eb8c8
Ali Hassan <hassan.jutt646464@gmail.com>
2024-11-07 23:28:55 +0500

    post api

--- lib/MVVM/data/network/base_api_services.dart
+++ lib/MVVM/data/network/base_api_services.dart
@@ -1,3 +1,4 @@
 abstract class BaseApiServices {
   Future<dynamic> getAPi(String url);
+  Future<dynamic> postAPi(dynamic data, String url);
 }
--- lib/MVVM/data/network/network_api_services.dart
+++ lib/MVVM/data/network/network_api_services.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:getx/MVVM/data/app_exceptions.dart';
 import 'package:getx/MVVM/data/network/base_api_services.dart';
 import 'package:http/http.dart' as http;
@@ -22,10 +23,36 @@ class NetworkApiServices extends BaseApiServices {
     return jsonResponse;
   }
 
+  @override
+  Future<dynamic> postAPi(var data, String url) async {
+    if (kDebugMode) {
+      print(url);
+      print(data);
+    }
+    dynamic jsonResponse;
+    try {
+      final response = await http
+          .post(Uri.parse(url), body: jsonEncode(data))
+          .timeout(Duration(seconds: 10));
+      jsonResponse = returnResponse(response);
+    } on SocketException {
+      throw InternetException('');
+    } on RequestTimeOut {
+      throw RequestTimeOut('');
+    }
+    return jsonResponse;
+  }
+
   dynamic returnResponse(http.Response response) {
     switch (response.statusCode) {
       case 200:
         dynamic jsonResponse = jsonDecode(response.body);
+        return jsonResponse;
+      case 400:
+        throw InvalidUrlException();
+      default:
+        throw FetchDataException(
+            'Error during communication' + response.statusCode.toString());
     }
   }
 }
