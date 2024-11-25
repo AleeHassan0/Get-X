c341adc924aaa55be34bd29a4ddfeb53e6966ae3
Ali Hassan <hassan.jutt646464@gmail.com>
2024-11-25 19:37:29 +0500

    xyz

--- lib/MVVM/res/components/internet_exception_widget.dart
+++ lib/MVVM/res/components/internet_exception_widget.dart
@@ -28,7 +28,7 @@ class _InternetExceptionWidgetState extends State<InternetExceptionWidget> {
                 textAlign: TextAlign.center,
               ),
               SizedBox(
-                height: height * 0.01,
+                height: height * 0.011,
               ),
               Container(
                 height: 42,
